43d395a587ac73b315d02d3b158d437fd23fd9a0
alireza tavakoli <alirezatavakoli99@gmail.com>
2017-11-09 14:08:05 +0200

    uusi kansio

---
+++ testikansio2/ajaxkoe2.php
@@ -0,0 +1,25 @@
+
+<?php 
+
+$host ="magnesium"; 
+$user = "a16atavakoli"; 
+$pass = "jere123"; 
+$dbname = "ska16atavakoli"; 
+
+try  //yritetään ottaa yhteys 
+{  
+    $yhteys = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass); //luo PDO-olion 
+}  
+catch(PDOException $e) // jos ei onnistu (poikkeus) 
+{  
+    echo $e->getMessage(); //antaa ilmoituksen siitä, missä virhe 
+} 
+
+$sql = "SELECT * FROM pelaajat"; 
+$vastaus = $yhteys->prepare($sql); 
+$vastaus->execute(); 
+$vastaukset = $vastaus->fetchAll(PDO::FETCH_ASSOC); 
+
+echo json_encode($vastaukset);
+
+?> 
